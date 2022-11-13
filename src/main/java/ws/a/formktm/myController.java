62e08a3bf7818daaacd05429b887c881176e5c2c
ERDA WYNE <erdawynee1@gmail.com>
2022-11-14 03:23:56 +0700

    membuat code di controller

--- src/main/java/ws/a/formktm/myController.java
+++ src/main/java/ws/a/formktm/myController.java
@@ -5,9 +5,13 @@
  */
 package ws.a.formktm;
 
+
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -15,6 +19,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class myController {
-     
+    @RequestMapping ("/identitas")
+    @ResponseBody
+    public String getData(@RequestParam("gambar") MultipartFile gambar,
+            @RequestParam("nama") String text,
+            @RequestParam("nim") String number,
+            @RequestParam("prodi") String text,
+            @RequestParam("email") String text
+            )
+            
+    String blob = Base64.encodeBase64String(gambar.getBytes());
     
+    text = textProcess(text);
+    
+    "<br><img src='data:image/jpeg;base64," +blob+"  '/><br>" + return text;
+            
+            
+    
+    
+    }
+    
+     
+    private String textProcess(String nama) {
+    String result = "";
+    if (nama.equals("Erda")) {result = nama +"Mahasiswa TI";}
+        else if(nama.equals("Wyne")) {result = nama + "Mahasiswa";}
+        else if(nama.equals("Astuti")) {result = nama + "Mahasiswa";}
+        else {result = nama + "cakep amat";
+        }
+        return result;
+    }
 }
